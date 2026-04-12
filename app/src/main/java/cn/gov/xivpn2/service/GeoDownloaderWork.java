a4f1bd39db9552b12b90310372be351fabc60bb0
Exclude0122 <173652916+Exclude0122@users.noreply.github.com>
2026-04-12 13:23:07 -0400

    rewrite geo database download

--- app/src/main/java/cn/gov/xivpn2/service/GeoDownloaderWork.java
+++ app/src/main/java/cn/gov/xivpn2/service/GeoDownloaderWork.java
@@ -1,16 +1,10 @@
 package cn.gov.xivpn2.service;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.ServiceInfo;
-import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-import androidx.work.ForegroundInfo;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
@@ -22,8 +16,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.concurrent.TimeUnit;
 
-import cn.gov.xivpn2.NotificationID;
-import cn.gov.xivpn2.R;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -32,168 +24,99 @@ public class GeoDownloaderWork extends Worker {
 
     private final static String TAG = "GeoDownloadWork";
 
+    private boolean interuppted = false;
 
     public GeoDownloaderWork(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
+
 
     @NonNull
     @Override
     public Result doWork() {
         Log.i(TAG, "do work");
 
-        // start foreground service
+        Data inputData = getInputData();
+        String url = inputData.getString("URL");
+        String path = inputData.getString("PATH");
 
-        Notification foregroundNotification = new NotificationCompat.Builder(getApplicationContext(), "XiVPNService")
-                .setSilent(true)
-                .setProgress(100, 0, true)
-                .setContentTitle(getApplicationContext().getString(R.string.downloading_geo_assets))
-                .setOngoing(true)
-                .setSmallIcon(R.drawable.baseline_refresh_24)
-                .build();
-
-        int id = NotificationID.getID();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            setForegroundAsync(new ForegroundInfo(id, foregroundNotification, ServiceInfo.FOREGROUND_SERVICE_TYPE_SHORT_SERVICE));
-        } else {
-            setForegroundAsync(new ForegroundInfo(id, foregroundNotification));
+        if (url == null || path == null || url.isEmpty() || path.isEmpty()) {
+            return Result.failure();
         }
 
-        SharedPreferences sp = getApplicationContext().getSharedPreferences("XIVPN", Context.MODE_PRIVATE);
-        String geoip = sp.getString("GEOIP", "v2fly");
-        String geosite = sp.getString("GEOSITE", "v2fly");
-
-        String geoipUrl = "";
-        String geositeUrl = "";
-
-        switch (geoip) {
-            case "v2fly":
-                geoipUrl = "https://github.com/v2fly/geoip/releases/latest/download/geoip.dat";
-                break;
-            case "v2fly_cn":
-                geoipUrl = "https://github.com/v2fly/geoip/releases/latest/download/geoip-only-cn-private.dat";
-                break;
-            case "ipinfo":
-                geoipUrl = "https://github.com/Exclude0122/geoip/releases/latest/download/geoip.dat";
-                break;
-            case "ipinfo_cn":
-                geoipUrl = "https://github.com/Exclude0122/geoip/releases/latest/download/geoip-only-cn-private.dat";
-                break;
-            case "ipinfo_ru":
-                geoipUrl = "https://github.com/Exclude0122/geoip/releases/latest/download/geoip-only-ru-private.dat";
-                break;
-            case "ipinfo_ir":
-                geoipUrl = "https://github.com/Exclude0122/geoip/releases/latest/download/geoip-only-ir-private.dat";
-                break;
-            case "custom":
-                geoipUrl = sp.getString("GEOIP_CUSTOM", "");
-                break;
-        }
-
-        switch (geosite) {
-            case "v2fly":
-                geositeUrl = "https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat";
-                break;
-            case "custom":
-                geositeUrl = sp.getString("GEOSITE_CUSTOM", "");
-                break;
-        }
-
-        Log.i(TAG, "ip " + geoipUrl);
-        Log.i(TAG, "site " + geositeUrl);
 
         OkHttpClient client = new OkHttpClient.Builder()
-                .readTimeout(5, TimeUnit.SECONDS)
-                .writeTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(2, TimeUnit.SECONDS)
+                .writeTimeout(2, TimeUnit.SECONDS)
                 .connectTimeout(5, TimeUnit.SECONDS)
                 .build();
 
+
+        setProgressAsync(new Data.Builder().putInt("progress", 0).putBoolean("indeterminate", true).build());
+
         try {
 
-            download(id, client, geoipUrl, new File(getApplicationContext().getFilesDir(), "geoip.dat"));
-            download(id, client, geositeUrl, new File(getApplicationContext().getFilesDir(), "geosite.dat"));
+            File file = new File(path);
+            Log.i(TAG, "download " + url + " => " + file.getAbsolutePath());
 
-        } catch (Exception e) {
+            Request request = new Request.Builder().url(url).build();
+
+            Response response = client.newCall(request).execute();
+
+            if (response.code() != 200) {
+                response.close();
+                throw new IOException("unexpected status code: " + response.code());
+            }
+
+            int totalLength = -1;
+            String contentLengthHeader = response.header("Content-Length");
+            if (contentLengthHeader != null) {
+                totalLength = Integer.parseInt(contentLengthHeader);
+            }
+
+            OutputStream outputStream = FileUtils.newOutputStream(file, false);
+            byte[] buffer = new byte[4096];
+
+            long lastUpdateProgress = 0;
+            int length = 0;
+            while (!interuppted) {
+                InputStream inputStream = response.body().byteStream();
+                int n = inputStream.read(buffer);
+                if (n <= 0) break;
+
+                length += n;
+                if (System.currentTimeMillis() - lastUpdateProgress > 100) {
+                    int progress = (int) (Math.floor((double) length / totalLength * 100));
+                    setProgressAsync(new Data.Builder().putInt("progress", progress).putBoolean("indeterminate", totalLength == -1).build());
+                    lastUpdateProgress = System.currentTimeMillis();
+                }
+
+                outputStream.write(buffer, 0, n);
+                outputStream.flush();
+            }
+
+            Thread.sleep(500);
+
+            setProgressAsync(new Data.Builder().putInt("progress", 100).putBoolean("indeterminate", false).build());
+
+            outputStream.close();
+
+            response.close();
+
+        } catch (IOException e) {
             Log.e(TAG, "download error", e);
 
-            // post error message
-            Notification notification = new NotificationCompat.Builder(getApplicationContext(), "XiVPNSubscriptions")
-                    .setSilent(true)
-                    .setContentTitle(getApplicationContext().getString(R.string.downloading_error))
-                    .setContentText(e.getClass().getSimpleName() + ": " + e.getMessage())
-                    .setSmallIcon(R.drawable.baseline_error_24)
-                    .build();
-            getApplicationContext().getSystemService(NotificationManager.class).notify(NotificationID.getID(), notification);
-
-            return Result.failure();
+            return Result.failure(new Data.Builder().putString("error", e.getClass().getSimpleName() + ": " + e.getMessage()).build());
+        } catch (InterruptedException e) {
+            // ignored
         }
 
         return Result.success();
     }
 
-    private void download(int id, OkHttpClient client, String url, File file) throws IOException {
-        Log.i(TAG, "download " + url + " => " + file.getAbsolutePath());
-
-        Request request = new Request.Builder().url(url).build();
-
-        Response response = client.newCall(request).execute();
-
-        if (response.code() != 200) {
-            response.close();
-            throw new IOException("unexpected status code: " + response.code());
-        }
-
-        if (response.body() == null) {
-            response.close();
-            throw new NullPointerException("null body");
-        }
-
-        int totalLength = -1;
-        String contentLengthHeader = response.header("Content-Length");
-        if (contentLengthHeader != null) {
-            totalLength = Integer.parseInt(contentLengthHeader);
-        }
-
-        OutputStream outputStream = FileUtils.newOutputStream(file, false);
-        byte[] buffer = new byte[4096];
-
-        long lastUpdateProgress = 0;
-        int length = 0;
-        while (true) {
-            InputStream inputStream = response.body().byteStream();
-            int n = inputStream.read(buffer);
-            if (n <= 0) break;
-
-            length += n;
-            if (System.currentTimeMillis() - lastUpdateProgress > 500) {
-                int progress = (int) (Math.floor((double) length / totalLength * 100));
-                updateProgress(id, "Downloading " + file.getName() + "...", progress, totalLength == -1);
-                lastUpdateProgress = System.currentTimeMillis();
-            }
-
-            outputStream.write(buffer, 0, n);
-            outputStream.flush();
-        }
-
-        outputStream.close();
-
-        response.close();
+    @Override
+    public void onStopped() {
+        interuppted = true;
+        super.onStopped();
     }
-
-    private void updateProgress(int id, String content, int progress, boolean indeterminate) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "XiVPNSubscriptions")
-                .setSilent(true)
-                .setProgress(100, progress, indeterminate)
-                .setContentTitle(getApplicationContext().getString(R.string.downloading_geo_assets))
-                .setOngoing(true)
-                .setSmallIcon(R.drawable.baseline_refresh_24);
-
-        if (content != null) {
-            builder.setContentText(content);
-        }
-
-        NotificationManager notificationManager = getApplicationContext().getSystemService(NotificationManager.class);
-        notificationManager.notify(id, builder.build());
-    }
-
 }
--- app/src/main/java/cn/gov/xivpn2/ui/GeoAssetsActivity.java
+++ app/src/main/java/cn/gov/xivpn2/ui/GeoAssetsActivity.java
@@ -1,27 +1,38 @@
 package cn.gov.xivpn2.ui;
 
-import android.content.SharedPreferences;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.RadioGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatEditText;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.work.Data;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.Operation;
 import androidx.work.OutOfQuotaPolicy;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+
+import java.io.File;
 import java.util.List;
-import java.util.Objects;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import cn.gov.xivpn2.R;
@@ -29,11 +40,8 @@ import cn.gov.xivpn2.service.GeoDownloaderWork;
 
 public class GeoAssetsActivity extends AppCompatActivity {
 
-    private RadioGroup groupGeoip;
-    private RadioGroup groupGeosite;
-    private AppCompatEditText editGeoip;
-    private AppCompatEditText editGeosite;
-    private boolean disableListeners = false;
+
+    private final String TAG = "GeoAssetsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,24 +57,212 @@ public class GeoAssetsActivity extends AppCompatActivity {
             return insets;
         });
 
-        groupGeoip = findViewById(R.id.group_geoip);
-        groupGeosite = findViewById(R.id.group_geosite);
-        editGeoip = findViewById(R.id.edit_custom_geoip);
-        editGeosite = findViewById(R.id.edit_custom_geosite);
 
-        groupGeosite.setOnCheckedChangeListener((group, checkedId) -> {
-            if (disableListeners) return;
-            save();
-            load();
+        MaterialButton btnGeoip = findViewById(R.id.geoip_download);
+        btnGeoip.setOnClickListener(v -> {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.download)
+                    .setItems(R.array.geoip, (dialog, which) -> {
+                        String url = "";
+                        switch (which) {
+                            case 0:
+                                url = "https://github.com/v2fly/geoip/releases/latest/download/geoip.dat";
+                                break;
+                            case 1:
+                                url = "https://github.com/v2fly/geoip/releases/latest/download/geoip-only-cn-private.dat";
+                                break;
+                            case 2:
+                                url = "https://github.com/Exclude0122/geoip/releases/latest/download/geoip.dat";
+                                break;
+                            case 3:
+                                url = "https://github.com/Exclude0122/geoip/releases/latest/download/geoip-only-cn-private.dat";
+                                break;
+                            case 4:
+                                url = "https://github.com/Exclude0122/geoip/releases/latest/download/geoip-only-ru-private.dat";
+                                break;
+                            case 5:
+                                url = "https://github.com/Exclude0122/geoip/releases/latest/download/geoip-only-ir-private.dat";
+                                break;
+                            case 6:
+                                url = "https://github.com/Exclude0122/geoip/releases/latest/download/geoip-only-kp-private.dat";
+                                break;
+                        }
+                        startDownload(url, "geoip.dat");
+                    })
+                    .show();
         });
 
-        groupGeoip.setOnCheckedChangeListener((group, checkedId) -> {
-            if (disableListeners) return;
-            save();
-            load();
+        MaterialButton btnGeosite = findViewById(R.id.geosite_download);
+        btnGeosite.setOnClickListener(v -> {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.download)
+                    .setItems(R.array.geosite, (dialog, which) -> {
+                        String url = "";
+                        switch (which) {
+                            case 0:
+                                url = "https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat";
+                                break;
+                            case 1:
+                                url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat";
+                                break;
+                            case 2:
+                                url = "https://github.com/Chocolate4U/Iran-v2ray-rules/releases/latest/download/geosite.dat";
+                                break;
+                            case 3:
+                                url = "https://github.com/Chocolate4U/Iran-v2ray-rules/releases/latest/download/geosite-lite.dat";
+                                break;
+                            case 4:
+                                url = "https://github.com/runetfreedom/russia-v2ray-rules-dat/releases/latest/download/geosite.dat";
+                                break;
+                        }
+                        startDownload(url, "geosite.dat");
+                    })
+                    .show();
         });
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) supportActionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setMargin(View view) {
+        int marginInDp = 16;
+        int marginInPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, marginInDp, getResources().getDisplayMetrics());
+
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) view.getLayoutParams();
+        params.setMargins(marginInPx, marginInPx, marginInPx, marginInPx);
+        view.setLayoutParams(params);
+    }
+
+    private void startDownload(String url, String file) {
+        try {
+            WorkManager workManager = WorkManager.getInstance(this);
+            List<WorkInfo> works = workManager.getWorkInfosByTag("geoassets").get();
+
+            boolean running = false;
+            for (WorkInfo work : works) {
+                if (work.getState() == WorkInfo.State.RUNNING || work.getState() == WorkInfo.State.ENQUEUED) {
+                    running = true;
+                    break;
+                }
+            }
+
+            if (running) {
+                Toast.makeText(this, R.string.already_updating_geo_assets, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+            OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(GeoDownloaderWork.class)
+                    .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                    .addTag("geoassets")
+                    .setInputData(new Data.Builder().putString("URL", url).putString("PATH", new File(getFilesDir(), file).getAbsolutePath()).build())
+                    .build();
+
+            LinearProgressIndicator progressBar = new LinearProgressIndicator(this);
+            progressBar.setIndeterminate(true);
+            FrameLayout frameLayout = new FrameLayout(this);
+            frameLayout.addView(progressBar);
+            setMargin(progressBar);
+
+            AlertDialog alertDialog = new MaterialAlertDialogBuilder(this)
+                    .setTitle(R.string.downloading)
+                    .setView(frameLayout)
+                    .setCancelable(false)
+                    .setNegativeButton(R.string.cancel, (dialog, which) -> {
+                        workManager.getWorkInfoById(workRequest.getId()).cancel(true);
+                    })
+                    .create();
+
+            alertDialog.show();
+
+
+            // start work
+
+            Operation operation = workManager.enqueue(workRequest);
+            operation.getResult().addListener(() -> {
+                UUID workId = workRequest.getId();
+                workManager.getWorkInfoByIdLiveData(workId)
+                        .observe(this, workInfo -> {
+                            WorkInfo.State state = workInfo.getState();
+                            if (state == WorkInfo.State.RUNNING) {
+
+                                Data progressData = workInfo.getProgress();
+                                int progress = progressData.getInt("progress", 0);
+                                boolean indeterminate = progressData.getBoolean("indeterminate", true);
+
+                                Log.d(TAG, "progress " + progress + " " + indeterminate);
+
+                                if (indeterminate) {
+                                    progressBar.setIndeterminate(true);
+                                } else {
+                                    progressBar.setIndeterminate(false);
+                                    progressBar.setProgress(progress);
+                                    progressBar.setMax(100);
+                                }
+
+                            } else if (state == WorkInfo.State.FAILED) {
+                                Toast.makeText(this, R.string.downloading_error, Toast.LENGTH_SHORT).show();
+                                alertDialog.dismiss();
+                                refresh();
+                            } else if (state == WorkInfo.State.SUCCEEDED) {
+                                Toast.makeText(this, R.string.download_success, Toast.LENGTH_SHORT).show();
+                                alertDialog.dismiss();
+                                refresh();
+                            }
+                        });
+            }, getMainExecutor());
+
+        } catch (ExecutionException | InterruptedException e) {
+            Log.e(TAG, "enqueue work", e);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        refresh();
+    }
+
+    private String prettyPrintLastModified(long lastModified) {
+        if (lastModified <= 0) return "Unknown";
+        if (lastModified > System.currentTimeMillis()) return "Future";
+
+        long duration = System.currentTimeMillis() - lastModified;
+
+        if (duration < 1000) return "Just now";
+        if (duration < 60 * 1000) return duration / 1000 + " seconds ago";
+        if (duration < 60 * 60 * 1000) return duration / 60 / 1000 + " minutes ago";
+        if (duration < 24 * 60 * 60 * 1000) return duration / 60 / 60 / 1000 + " hours ago";
+        return duration / 24 / 60 / 60 / 1000 + " days ago";
+    }
+
+    @SuppressLint({"SetTextI18n", "DefaultLocale"})
+    private void refresh() {
+        TextView geoipSize = findViewById(R.id.geoip_size);
+        TextView geoipDate = findViewById(R.id.geoip_date);
+        TextView geositeSize = findViewById(R.id.geosite_size);
+        TextView geositeDate = findViewById(R.id.geosite_date);
+
+        File filesDir = getFilesDir();
+        File geoip = new File(filesDir, "geoip.dat");
+        File geosite = new File(filesDir, "geosite.dat");
+
+        if (geoip.exists()) {
+            geoipSize.setText((String.format("%.2f MB", ((float) geoip.length()) / 1024.0 / 1024.0)));
+            geoipDate.setText(prettyPrintLastModified(geoip.lastModified()));
+        } else {
+            geoipSize.setText(R.string.not_found);
+            geoipDate.setText("");
+        }
+
+        if (geosite.exists()) {
+            geositeSize.setText((String.format("%.2f MB", ((float) geosite.length()) / 1024.0 / 1024.0)));
+            geositeDate.setText(prettyPrintLastModified(geosite.lastModified()));
+        } else {
+            geositeSize.setText(R.string.not_found);
+            geositeDate.setText("");
+        }
     }
 
     @Override
@@ -74,81 +270,10 @@ public class GeoAssetsActivity extends AppCompatActivity {
         if (item.getItemId() == android.R.id.home) {
             this.finish();
             return true;
-        } else if (item.getItemId() == R.id.update) {
-            try {
-                WorkManager workManager = WorkManager.getInstance(this);
-                List<WorkInfo> works = workManager.getWorkInfosByTag("geoassets").get();
-
-                boolean running = false;
-                for (WorkInfo work : works) {
-                    if (work.getState() == WorkInfo.State.RUNNING || work.getState() == WorkInfo.State.ENQUEUED) {
-                        running = true;
-                        break;
-                    }
-                }
-
-                if (!running) {
-                    workManager.enqueue(
-                            new OneTimeWorkRequest.Builder(GeoDownloaderWork.class)
-                                    .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
-                                    .addTag("geoassets")
-                                    .build()
-                    );
-                }
-
-
-            } catch (ExecutionException | InterruptedException e) {
-                Log.e("GeoAssetsActivity", "enqueue work", e);
-            }
-
-            return true;
         }
         return false;
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        load();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        save();
-    }
-
-    private void save() {
-        SharedPreferences sp = getSharedPreferences("XIVPN", MODE_PRIVATE);
-        SharedPreferences.Editor edit = sp.edit();
-
-        if (groupGeoip.getCheckedRadioButtonId() == R.id.v2fly_geoip) {
-            edit.putString("GEOIP", "v2fly");
-        } else if (groupGeoip.getCheckedRadioButtonId() == R.id.v2fly_geoip_cn) {
-            edit.putString("GEOIP", "v2fly_cn");
-        } else if (groupGeoip.getCheckedRadioButtonId() == R.id.ipinfo_geoip) {
-            edit.putString("GEOIP", "ipinfo");
-        } else if (groupGeoip.getCheckedRadioButtonId() == R.id.ipinfo_geoip_cn) {
-            edit.putString("GEOIP", "ipinfo_cn");
-        } else if (groupGeoip.getCheckedRadioButtonId() == R.id.ipinfo_geoip_ir) {
-            edit.putString("GEOIP", "ipinfo_ir");
-        } else if (groupGeoip.getCheckedRadioButtonId() == R.id.ipinfo_geoip_ru) {
-            edit.putString("GEOIP", "ipinfo_ru");
-        } else if (groupGeoip.getCheckedRadioButtonId() == R.id.geoip_custom) {
-            edit.putString("GEOIP", "custom");
-        }
-        edit.putString("GEOIP_CUSTOM", Objects.requireNonNull(editGeoip.getText()).toString());
-
-
-        if (groupGeosite.getCheckedRadioButtonId() == R.id.v2lfy_dlc) {
-            edit.putString("GEOSITE", "v2fly");
-        } else if (groupGeosite.getCheckedRadioButtonId() == R.id.geosite_custom) {
-            edit.putString("GEOSITE", "custom");
-        }
-        edit.putString("GEOSITE_CUSTOM", Objects.requireNonNull(editGeosite.getText()).toString());
-
-        edit.apply();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -156,56 +281,5 @@ public class GeoAssetsActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-
-    private void load() {
-        disableListeners = true;
-
-        SharedPreferences sp = getSharedPreferences("XIVPN", MODE_PRIVATE);
-        String geoip = sp.getString("GEOIP", "v2fly");
-        String geosite = sp.getString("GEOSITE", "v2fly");
-
-        switch (geoip) {
-            case "v2fly":
-                groupGeoip.check(R.id.v2fly_geoip);
-                break;
-            case "v2fly_cn":
-                groupGeoip.check(R.id.v2fly_geoip_cn);
-                break;
-            case "ipinfo":
-                groupGeoip.check(R.id.ipinfo_geoip);
-                break;
-            case "ipinfo_cn":
-                groupGeoip.check(R.id.ipinfo_geoip_cn);
-                break;
-            case "ipinfo_ru":
-                groupGeoip.check(R.id.ipinfo_geoip_ru);
-                break;
-            case "ipinfo_ir":
-                groupGeoip.check(R.id.ipinfo_geoip_ir);
-                break;
-        }
-
-        editGeoip.setText(sp.getString("GEOIP_CUSTOM", ""));
-        if (geoip.equals("custom")) {
-            groupGeoip.check(R.id.geoip_custom);
-            editGeoip.setVisibility(View.VISIBLE);
-        } else {
-            editGeoip.setVisibility(View.GONE);
-        }
-
-        if (geosite.equals("v2fly")) {
-            groupGeosite.check(R.id.v2lfy_dlc);
-        }
-
-        editGeosite.setText(sp.getString("GEOSITE_CUSTOM", ""));
-        if (geosite.equals("custom")) {
-            editGeosite.setVisibility(View.VISIBLE);
-            groupGeosite.check(R.id.geosite_custom);
-        } else {
-            editGeosite.setVisibility(View.GONE);
-        }
-
-        disableListeners = false;
-    }
 
 }
